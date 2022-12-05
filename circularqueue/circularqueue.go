d66942eeddaac72f2a05392d32ff75deee13c844
mfbsouza <mfbsouza.it@gmail.com>
2022-12-05 12:30:57 -0300

    circularqueue.go: updated documentation and minor changes in the Remaining method
    Signed-off-by: mfbsouza <mfbsouza.it@gmail.com>

--- circularqueue/circularqueue.go
+++ circularqueue/circularqueue.go
@@ -1,12 +1,10 @@
-// Package circularqueue provides a Circular Buffer
-// implementation following the FIFO algorithm
+// Package circularqueue provides a FIFO algorithm implementation
 package circularqueue
 
-// Queue interface represents the common operations
-// of a circular queue
+// Queue interface represents the common operations of a FIFO object
 type Queue interface {
-	Empty() bool
-	Full() bool
+	IsEmpty() bool
+	IsFull() bool
 	Enqueue(interface{}) bool
 	Dequeue() interface{}
 	Peek() interface{}
@@ -21,9 +19,8 @@ type Queue_t struct {
 	tail     int
 }
 
-// function New initializes a new Queue data type
-// It will return nil in case of a error
-// or a initialized Queue object
+// function New initializes a new Queue object.
+// This method returns nil in case of a error or a initialized Queue object
 func New(size int) *Queue_t {
 	if size <= 0 {
 		return nil
@@ -37,31 +34,31 @@ func New(size int) *Queue_t {
 	}
 }
 
-// The Empty method checks if a given Queue_t is empty
-func (p *Queue_t) Empty() bool {
+// IsEmpty method checks if a given Queue object is empty
+func (p *Queue_t) IsEmpty() bool {
 	if p.head == -1 && p.tail == -1 {
 		return true
 	}
 	return false
 }
 
-// The Full method checks if a given Queue_t is full
-func (p *Queue_t) Full() bool {
+// IsFull method checks if a given Queue object is full
+func (p *Queue_t) IsFull() bool {
 	if ((p.head + 1) % p.capacity) == p.tail {
 		return true
 	}
 	return false
 }
 
-// The Enqueue method adds a given v value to the queue
-// and returns if the value was added successfully or not
+// Enqueue method adds a given v value to the Queue object.
+// This method returns if the value was successfully added or not
 func (p *Queue_t) Enqueue(v interface{}) bool {
-	if p.Empty() {
+	if p.IsEmpty() {
 		p.head = 0
 		p.tail = 0
 		p.elmts[p.head] = v
 		return true
-	} else if p.Full() {
+	} else if p.IsFull() {
 		return false
 	} else {
 		p.head = (p.head + 1) % p.capacity
@@ -70,11 +67,10 @@ func (p *Queue_t) Enqueue(v interface{}) bool {
 	}
 }
 
-// The Dequeue method removes a value from the queue
-// and returns this removed value or a nil in case
-// of a empty queue
+// Dequeue method removes a value from the Queue object.
+// This method returns nil in case of a empty Queue or a value
 func (p *Queue_t) Dequeue() (v interface{}) {
-	if p.Empty() {
+	if p.IsEmpty() {
 		v = nil
 	} else {
 		v = p.elmts[p.tail]
@@ -88,23 +84,20 @@ func (p *Queue_t) Dequeue() (v interface{}) {
 	return
 }
 
-// The Peek method returns the next value
-// to be removed from the queue or nil
+// Peek method returns the next value to be dequeued.
+// This method returns nil in canse of a empty Queue or a value
 func (p *Queue_t) Peek() interface{} {
-	if p.Empty() {
+	if p.IsEmpty() {
 		return nil
 	} else {
 		return p.elmts[p.tail]
 	}
 }
 
-// The Remaining method returns how many elements are
-// left empty to be used in the Queue
+// Remaining method returns how many free slots are left in the Queue object
 func (p *Queue_t) Remaining() (cnt int) {
-	if p.Empty() {
+	if p.IsEmpty() {
 		cnt = p.capacity
-	} else if p.Full() {
-		cnt = 0
 	} else {
 		for inter := p.head; ((inter + 1) % p.capacity) != p.tail; inter++ {
 			cnt++
